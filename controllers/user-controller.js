5d7cafb1876a9e61caf3b5f971b6d2fce1be9c49
Yusuf Msabeh <yussef.msabeh@gmail.com>
2023-06-04 16:37:09 +0300

    feat: handle books requests

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,7 +1,22 @@
+const fileSystem = require("../utils/file-system")
+const crypto = require("crypto");
 exports.getBooks = (request, response, next) => {
+    const books = fileSystem.readBooks();
+    response.json(books)
 };
 exports.getBookById = (request, response, next) => {
+    const {id} = request.params;
+    const book = fileSystem.readBooks().find((book) => book.id == id)
+    response.json(book);
 };
 exports.postBook = (request, response, next) => {
+    const {name} = request.body;
+    if (!name) return response.status(500).json({statusCode: 500, error: "Name filed required"});
+    const id = crypto.randomUUID();
+    const book = {id: id, name: name};
+    const books = fileSystem.readBooks();
+    books.push(book);
+    fileSystem.writeBooks(books);
+    response.json(book);
 };
 
--- utils/file-system.js
+++ utils/file-system.js
@@ -1,4 +1,13 @@
-const fs= require("fs");
+const fs = require("fs");
 const path = require("path");
 
+const FILEPATH = path.join(__dirname, "..", "data", "books.json");
+exports.readBooks = () => {
+    const data = fs.readFileSync(FILEPATH, "utf8");
+    return JSON.parse(data);
+}
 
+exports.writeBooks = (data) => {
+ console.log(data);
+    fs.writeFileSync(FILEPATH, JSON.stringify(data))
+}
